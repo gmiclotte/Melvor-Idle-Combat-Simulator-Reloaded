52961b6f00c767eff6508782acad1390ea9e32e2
Visua <63650+visua0@users.noreply.github.com>
2020-10-02 13:27:21 +0200

    v0.11.2

--- Extension/sources/injectable/main.js
+++ Extension/sources/injectable/main.js
@@ -16,7 +16,7 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 (() => {
-  const version = 'v0.11.1';
+  const version = 'v0.11.2';
 
   /**
    * Container Class for the Combat Simulator.
