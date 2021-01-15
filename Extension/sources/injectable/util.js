fb1d833d6a1c80e1e498f3b54b8b5cec257c640f
Giles Miclotte <Giles.Miclotte@GMail.com>
2021-01-15 17:28:42 +0100

    Update pre-release v1.2.4-1

--- Extension/sources/injectable/util.js
+++ Extension/sources/injectable/util.js
@@ -2,7 +2,7 @@
 
     const MICSR = window.MICSR;
 
-    MICSR.version = 'v1.2.3';
+    MICSR.version = 'v1.2.4-1';
     MICSR.gameVersion = 'Alpha v0.18.2';
     MICSR.maxActions = 300;
     MICSR.trials = 10000;
