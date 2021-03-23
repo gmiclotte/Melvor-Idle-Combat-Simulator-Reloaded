aa5cccb9535640ab5a92e99b8aff064adb7bf338
Giles Miclotte <Giles.Miclotte@GMail.com>
2021-03-23 10:39:04 +0100

    Update version 1.3.4

--- Extension/sources/injectable/util.js
+++ Extension/sources/injectable/util.js
@@ -6,7 +6,7 @@
 
         const MICSR = window.MICSR;
 
-        MICSR.version = 'v1.3.3';
+        MICSR.version = 'v1.3.4';
         MICSR.gameVersion = 'Alpha v0.19.1';
         MICSR.maxActions = 300;
         MICSR.trials = 10000;
