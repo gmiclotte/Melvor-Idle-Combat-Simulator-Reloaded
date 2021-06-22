7a1d96e52a4b2425cef8757d6b811f348c542b91
Giles Miclotte <Giles.Miclotte@GMail.com>
2021-06-22 12:52:06 +0200

    Update version 1.4.1

--- Extension/sources/injectable/util.js
+++ Extension/sources/injectable/util.js
@@ -30,7 +30,7 @@
         // combat sim version
         MICSR.majorVersion = 1;
         MICSR.minorVersion = 4;
-        MICSR.patchVersion = 0;
+        MICSR.patchVersion = 1;
         MICSR.preReleaseVersion = undefined;
         MICSR.version = `v${MICSR.majorVersion}.${MICSR.minorVersion}.${MICSR.patchVersion}`;
         if (MICSR.preReleaseVersion !== undefined) {
